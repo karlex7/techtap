5c9fd7004a2803d3f538264ebff2424349ac1c92
Karlo Hren <KarloH@NGH.local>
2023-09-13 13:17:56 +0200

    Changed app.py changed database url

--- app.py
+++ app.py
@@ -123,4 +123,5 @@ def generate_qr(short_url):
     return send_file(stream, mimetype="image/png")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")#, port=5000)
+
